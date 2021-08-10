9f842ffc2fbd12ee9bb91f3aa9032c73569c6c69
Roberto Barros <roberto.f.barros@gmail.com>
2021-08-10 19:31:19 -0300

    livecode

---
+++ task.rb
@@ -0,0 +1,24 @@
+# MODEL TASK
+class Task
+  attr_reader :description
+
+  def initialize(description)
+    @description = description
+    @done = false
+  end
+
+  def done?
+    @done
+  end
+
+  def done!
+    @done = true
+  end
+end
+
+
+# t1 = Task.new('Aprender MVC')
+# t1.description # => 'Aprender MVC'
+# t1.done? # => false
+# t1.done!
+# t1.done? # => true
